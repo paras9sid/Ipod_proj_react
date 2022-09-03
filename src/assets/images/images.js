5a7b5c953e51d81ed80aadcc50fddc94ace11a73
paras9sid <sidharthjainp9@gmail.com>
2022-09-03 23:47:16 +0530

    Mini Ipod created in react checked

---
+++ src/assets/images/images.js
@@ -0,0 +1,28 @@
+import wallpaper_main from "./wallpaper_main.jpg";
+import wallpaper2 from "./wallpaper2.jpg";
+import wallpaper3 from "./wallpaper3.jpg";
+import wallpaper4 from "./wallpaper4.jpg";
+import wallpaper5 from "./wallpaper5.jpg";
+import coverflow from "./coverflow.png";
+import games from "./games.jpeg";
+import artists from "./artists.jpg";
+import albums from "./albums.png";
+import song1Img from "./despacito.jpg";
+import song2Img from "./Hostel-1.jpg";
+
+const images = {
+	wallpaper_main,
+	wallpaper2,
+	wallpaper3,
+	wallpaper4,
+	wallpaper5,
+	coverflow,
+	games,
+	artists,
+	albums,
+	song1Img,
+	song2Img,
+
+};
+
+export default images;
---
+++ src/assets/songs/songs.js
@@ -0,0 +1,9 @@
+import music1 from "./despacito.mp3";
+import music2 from "./hostel.mp3";
+
+const songs = {
+	music1,
+	music2
+};
+
+export default songs;
